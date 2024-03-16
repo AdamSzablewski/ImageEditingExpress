39ae7335f1d705cd204017033414a4cec3c97cde
AdamSzablewski <adamszablewski8@gmail.com>
2024-03-16 22:55:34 +0100

    update

--- src/main/java/com/example/imageeditingexpress/model/Effects.java
+++ src/main/java/com/example/imageeditingexpress/model/Effects.java
@@ -1,10 +1,7 @@
 package com.example.imageeditingexpress.model;
 
 import javafx.beans.property.DoubleProperty;
-import javafx.scene.effect.Blend;
-import javafx.scene.effect.BlendMode;
-import javafx.scene.effect.ColorAdjust;
-import javafx.scene.effect.GaussianBlur;
+import javafx.scene.effect.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,19 +9,44 @@ import lombok.Setter;
 @Setter
 public class Effects {
 
-    private ColorAdjust colorAdjust = new ColorAdjust();
-    private GaussianBlur gaussianBlur = new GaussianBlur(0);
-    private Blend blend;
+    private ColorAdjust colorAdjust;
+    private GaussianBlur gaussianBlur;
+    private Bloom bloom;
+    private MotionBlur motionBlur;
 
     public Effects() {
-        Blend blend = new Blend();
-
-        blend.setTopInput(colorAdjust);
-        blend.setBottomInput(gaussianBlur);
-        this.blend = blend;
+        this.colorAdjust = new ColorAdjust();
+        this.gaussianBlur = new GaussianBlur(0);
+        this.bloom = new Bloom(1);
+        this.motionBlur = new MotionBlur();
+        this.motionBlur.setRadius(0);
     }
-
+    public Effect getEffectsForImageView(){
+        bloom.setInput(motionBlur);
+        gaussianBlur.setInput(bloom);
+        colorAdjust.setInput(gaussianBlur);
+        return colorAdjust;
+    }
     public void changeGaussianBlur(DoubleProperty valueProperty) {
         gaussianBlur.radiusProperty().bind(valueProperty);
     }
+    public void changeMotionBlur(DoubleProperty valueProperty) {
+        motionBlur.radiusProperty().bind(valueProperty);
+    }
+    public void changeBloom(double value) {
+        bloom.setThreshold(value);
+    }
+    public void changeSaturation(double value){
+        colorAdjust.setSaturation(value);
+    }
+    public void changeHue(double value){
+        colorAdjust.setHue(value);
+    }
+    public void changeBrightness(double value){
+        colorAdjust.setBrightness(value);
+    }
+    public void changeContrast(double value){
+        colorAdjust.setContrast(value);
+    }
+
 }
--- src/main/java/com/example/imageeditingexpress/service/ImageManipulator.java
+++ src/main/java/com/example/imageeditingexpress/service/ImageManipulator.java
@@ -4,19 +4,12 @@ import com.example.imageeditingexpress.model.Effects;
 import com.example.imageeditingexpress.util.ImageRotator;
 import javafx.fxml.FXML;
 import javafx.scene.control.Slider;
-import javafx.scene.effect.Bloom;
-import javafx.scene.effect.ColorAdjust;
-import javafx.scene.effect.GaussianBlur;
-import javafx.scene.effect.MotionBlur;
+import javafx.scene.effect.*;
 import javafx.scene.image.ImageView;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public class ImageManipulator {
-    private ColorAdjust colorAdjust;
-    private GaussianBlur gaussianBlur;
-    private Bloom bloom;
-    private MotionBlur motionBlur;
     @FXML
     private ImageView imageView;
     private ImageRotator imageRotator;
@@ -27,57 +20,48 @@ public class ImageManipulator {
         this.imageView = imageView;
         this.imageRotator = new ImageRotator();
         this.effects = new Effects();
-        this.colorAdjust = new ColorAdjust();
-        this.gaussianBlur = new GaussianBlur(0);
-        this.bloom = new Bloom(0);
-        this.motionBlur = new MotionBlur();
     }
+
 
     public void setImageView(ImageView imageView){
         this.imageView = imageView;
         this.imageRotator.setImageView(imageView);
-
-        bloom.setInput(motionBlur);
-        gaussianBlur.setInput(bloom);
-        colorAdjust.setInput(gaussianBlur);
-        this.imageView.setEffect(colorAdjust);
+        this.imageView.setEffect(effects.getEffectsForImageView());
     }
     public void handleSaturationChange(Slider saturationSlideBar) {
         saturationSlideBar.valueProperty().addListener((observable, oldValue, newValue) -> {
-            colorAdjust.setSaturation(newValue.doubleValue());
+            effects.changeSaturation(newValue.doubleValue());
         });
     }
     public void handleBrightnessChange(Slider brightnessSlideBar) {
         brightnessSlideBar.valueProperty().addListener((observable, oldValue, newValue) -> {
-            colorAdjust.setBrightness(newValue.doubleValue());
+            effects.changeBrightness(newValue.doubleValue());
         });
     }
     public void handleContrastChange(Slider contrastSlideBar) {
         contrastSlideBar.valueProperty().addListener((observable, oldValue, newValue) -> {
-            colorAdjust.setContrast(newValue.doubleValue());
+            effects.changeContrast(newValue.doubleValue());
         });
     }
     public void handleHueChange(Slider hueSlideBar) {
         hueSlideBar.valueProperty().addListener((observable, oldValue, newValue) -> {
-            colorAdjust.setHue(newValue.doubleValue());
+            effects.changeHue(newValue.doubleValue());
         });
     }
     public void handleBloomChange(Slider bloomSlideBar) {
         bloomSlideBar.valueProperty().addListener((observable, oldValue, newValue) -> {
-            bloom.setThreshold(newValue.doubleValue());
+            effects.changeBloom(newValue.doubleValue());
         });
     }
     public void handleBlurChange(Slider blurSlider) {
         blurSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
-            //effects.changeGaussianBlur(blurSlider.valueProperty());
-            gaussianBlur.radiusProperty().bind(blurSlider.valueProperty());
+            effects.changeGaussianBlur(blurSlider.valueProperty());
         });
 
     }
     public void handleMotionBlurChange(Slider motionSlideBar) {
         motionSlideBar.valueProperty().addListener((observable, oldValue, newValue) -> {
-            //effects.changeGaussianBlur(blurSlider.valueProperty());
-            motionBlur.radiusProperty().bind(motionSlideBar.valueProperty());
+            effects.changeMotionBlur(motionSlideBar.valueProperty());
         });
 
     }
